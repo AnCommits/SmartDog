52953370363244cabde8208d8034fa2df19e5d7a
AnUser99 <kisamasyanya@gmail.com>
2023-02-27 23:42:29 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-//        String inp = "Woof-woof-woof-woof-woof-woof-woof-woof! Woof-woof-woof-woof-woof-woof-woof-woof-woof!";
+        String inp = "Woof-woof-woof-woof-woof-woof-woof-woof! Woof-woof-woof-woof-woof-woof-woof-woof-woof!";
 //        String inp = "khkhkhWbzzzzobzzzOghrrrrfseswsw!";
 //        String inp = "Wofowof!";
 //        String inp = "Woofshhh! Shhh!";
-        String inp = "Woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof!";
+//        String inp = "Woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof!";
         String res = SmartDogOwner.woofDecoder(inp);
         System.out.println(res);
     }
--- src/SmartDogOwner.java
+++ src/SmartDogOwner.java
@@ -1,28 +1,28 @@
 public class SmartDogOwner {
 
     public static String woofDecoder(String str) {
+        java.util.function.Function<String, Integer> countWoofs = woofs -> {
+            java.util.function.Predicate<String> isValidWoof = woof -> {
+                int w = woof.indexOf('w');
+                int f = woof.lastIndexOf('f');
+                if (w < 0 || f <= w) return false;
+                String s = woof.substring(w, f);
+                return s.indexOf('o') != s.lastIndexOf('o');
+            };
+
+            int result = 0;
+            for (String woof : woofs.split("-")) {
+                if (isValidWoof.test(woof)) result++;
+            }
+            return result;
+        };
+
         StringBuilder result = new StringBuilder();
         for (String woofs : str.toLowerCase().split("!")) {
-            int n = countWoofs(woofs);
+            int n = countWoofs.apply(woofs);
             if (n > 26) return "just barking!";
             if (n > 0) result.append((char) ('a' - 1 + n));
         }
         return result.length() > 0 ? result.toString() : "nothing to decode!";
-    }
-
-    static int countWoofs(String woofs) {
-        int result = 0;
-        for (String woof : woofs.split("-")) {
-            result += isValidWoof(woof) ? 1 : 0;
-        }
-        return result;
-    }
-
-    static boolean isValidWoof(String woof) {
-        int w = woof.indexOf('w');
-        int f = woof.lastIndexOf('f');
-        if (w < 0 || f <= w) return false;
-        String s = woof.substring(w, f);
-        return s.indexOf('o') != s.lastIndexOf('o');
     }
 }
